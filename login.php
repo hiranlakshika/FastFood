03867cf012ed1e1e0f41cc62997d75e46ff65a5c
[Hiran Lakshika] <[hiranlakshika@gmail.com]>
2016-09-16 12:34:36 +0530

    Login verification completed

--- login.php
+++ login.php
@@ -1,3 +1,28 @@
 <?php
 
+include ("dbconnect.php");
+
+$uname = $_POST["user"];
+$pass = sha1($_POST["pass"]);
+
+$sql = "SELECT password FROM login WHERE username = '" . $uname . "'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        if (!strcmp($pass, $row["password"])) {
+            echo "<script type='text/javascript'>alert('Login Successfull!'); "
+            . "window.location.href='http://localhost/FastFood/index.html';</script>";
+        } else {
+            echo "<script type='text/javascript'>alert('Wrong password!'); "
+            . "window.location.href='http://localhost/FastFood/index.html';</script>";
+        }
+    }
+} else {
+    echo "<script type='text/javascript'>alert('User name not valid!'); "
+    . "window.location.href='http://localhost/FastFood/index.html';</script>";
+}
+$conn->close();
 ?>
