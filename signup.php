0778cb2fc3f5e0b45cd66863636694082d252542
[Hiran Lakshika] <[hiranlakshika@gmail.com]>
2016-09-16 16:17:20 +0530

    Sign up design db completed

--- signup.php
+++ signup.php
@@ -7,5 +7,21 @@ $signupPhone = $_POST["signupPhone"];
 $signupEmail = $_POST["signupEmail"];
 $signupPassword = sha1($_POST["signupPassword"]);
 
+$sql = "insert into user (name,email,phone) values ('$signupName','$signupEmail','$signupPhone')";
 
+if ($conn->query($sql) === TRUE) {
+
+    $sql = "INSERT INTO login (username, password)VALUES ('$signupName','$signupPassword')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script type='text/javascript'>alert('Sign up successful!'); "
+        . "window.location.href='http://localhost/FastFood/index.html';</script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
